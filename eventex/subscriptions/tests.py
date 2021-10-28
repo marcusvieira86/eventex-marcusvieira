e672d1e30595d3999161968f1528516be119a27c
Marcus Vieira <marcus.vieira@gmail.com>
2021-10-28 10:11:00 -0300

    Alinhado com o Sender entity

--- eventex/subscriptions/tests.py
+++ eventex/subscriptions/tests.py
@@ -59,13 +59,13 @@ class SubscribePostTest(TestCase):
 
     def test_subscription_email_from(self):
         email = mail.outbox[0]
-        expect = 'contato@eventex.com.br'
+        expect = 'marcus.vieira@gmail.com'
 
         self.assertEqual(expect, email.from_email)
 
     def test_subscription_email_to(self):
         email = mail.outbox[0]
-        expect = ['contato@eventex.com.br', 'henrique@bastos.net']
+        expect = ['marcus.vieira@gmail.com', 'henrique@bastos.net']
 
         self.assertEqual(expect, email.to)
 
--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -12,8 +12,8 @@ def subscribe(request):
         if form.is_valid():
             form.full_clean()
             body = render_to_string('subscriptions/subscription_email.txt', form.cleaned_data)
-            mail.send_mail('Confirmação de inscrição', body, 'contato@eventex.com.br',
-                           ['contato@eventex.com.br', form.cleaned_data['email']])
+            mail.send_mail('Confirmação de inscrição', body, 'marcus.vieira@gmail.com',
+                           ['marcus.vieira@gmail.com', form.cleaned_data['email']])
 
             messages.success(request, 'Inscrição realizada com sucesso!')
 
